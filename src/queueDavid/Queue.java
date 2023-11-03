8232bb4000d18a53d7062be5d8bb64b5512f98ff
JacktheRipper0911 <daviduseche09@gmail.com>
2023-11-03 16:10:39 -0500

    Cambios de prueba

--- src/queueDavid/Queue.java
+++ src/queueDavid/Queue.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 
 public class Queue {
     private LinkedList<Asistente> list = new LinkedList<>();
-    private String prueba;
+//    private String prueba;
 
     public void add(Asistente asistente) {
         this.list.add(asistente);
